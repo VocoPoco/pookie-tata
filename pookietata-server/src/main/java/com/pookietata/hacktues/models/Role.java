4ce591a609143a06f51ca76e4f954a380a17abf5
VocoPoco <VocoPoco@email.com>
2024-03-14 03:02:30 +0200

    created entry models

--- pookietata-server/src/main/java/com/pookietata/hacktues/models/Role.java
+++ pookietata-server/src/main/java/com/pookietata/hacktues/models/Role.java
@@ -1,2 +1,29 @@
-package com.pookietata.hacktues.models;public class Role {
+package com.pookietata.hacktues.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @ManyToMany(mappedBy = "users")
+    private Set<User> users;
 }
--- pookietata-server/src/main/java/com/pookietata/hacktues/models/User.java
+++ pookietata-server/src/main/java/com/pookietata/hacktues/models/User.java
@@ -1,4 +1,45 @@
-package src.main.java.com.pookietata.hacktues.models;
+package com.pookietata.hacktues.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "summoner_name", length = 100)
+    private String summonerName;
+
+    @Column(name = "currency_balance")
+    private Integer currencyBalance;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
