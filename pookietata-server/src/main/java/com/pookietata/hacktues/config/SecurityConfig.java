702a80f1b0061020fc076bb0c07c37525755273d
D14m0nDS <80215134+D14m0nDS@users.noreply.github.com>
2024-03-14 02:11:57 +0200

    fixed maven dependancies

--- pookietata-server/src/main/java/com/pookietata/hacktues/config/SecurityConfig.java
+++ pookietata-server/src/main/java/com/pookietata/hacktues/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.example.polls.config;
 
-import com.example.polls.security.CustomUserDetailsService;
-import com.example.polls.security.JwtAuthenticationEntryPoint;
-import com.example.polls.security.JwtAuthenticationFilter;
+import com.pookietata.hacktues.security.CustomUserDetailsService;
+import com.pookietata.hacktues.security.JwtAuthenticationEntryPoint;
+import com.pookietata.hacktues.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- pookietata-server/src/test/java/com/pookietata/hacktues/HackTuesApplicationTests.java
+++ pookietata-server/src/test/java/com/pookietata/hacktues/HackTuesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.viki.kanbanboard;
+package src.test.java.com.pookietata.hacktues;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
