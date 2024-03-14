7d8c307690743fa8961a608f9af39c1fbcaf0ea1
VocoPoco <VocoPoco@email.com>
2024-03-14 03:03:55 +0200

    created repositories for the models

--- pookietata-server/src/main/java/com/pookietata/hacktues/repositories/UserRepository.java
+++ pookietata-server/src/main/java/com/pookietata/hacktues/repositories/UserRepository.java
@@ -1,2 +1,19 @@
-package com.pookietata.hacktues.repositories;public class UserRepository {
+package com.pookietata.hacktues.repositories;
+
+
+import com.pookietata.hacktues.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    Optional<User> findUserByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findUserByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.summonerName = :summonerName")
+    Optional<User> findUserBySummonerName(String summonerName);
 }
